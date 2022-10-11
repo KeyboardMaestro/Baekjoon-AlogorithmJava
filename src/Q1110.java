880ed13c929bd5a943ecdc55800447fd22771628
judepark <psw0616a@naver.com>
2022-10-11 23:50:21 +0900

    11, Oct, 2022

---
+++ src/Q1110.java
@@ -0,0 +1,22 @@
+//Baekjoon Question 1110
+import java.util.*;
+
+public class Q1110 {
+    public static void main(String[] args) {
+        int inputNumber;
+        Scanner scanner = new Scanner(System.in);
+        inputNumber = scanner.nextInt();
+        int counter = 0;
+        int temp = 0;
+        int step = 0;
+        int nextstep = 0;
+                temp = inputNumber;
+                do {
+                    step = (temp/10) + (temp%10);
+                    nextstep = (temp%10 * 10) + step%10;
+                    temp = nextstep;
+                    counter++;
+                } while (temp != inputNumber);
+                System.out.println(counter);
+    }
+}
