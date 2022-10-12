f3fd12bbfa5cc94c8d22f2e51defeca4d9dc3fc2
judepark <psw0616a@naver.com>
2022-10-12 12:37:06 +0900

    Default Setting for this project

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,7 @@
 /// Beakjoon Question 15552
-import java.io.*;
-import java.util.Scanner;
-
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int numbers = scanner.nextInt();
-        for (int j = 0; j < numbers; j++) {
-            for (int i = 0; i <= j; i++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        //Empty
     }
 }
