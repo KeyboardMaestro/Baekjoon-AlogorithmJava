02bf40a597a87955333abca7f0359c8a5f940b44
judepark <psw0616a@naver.com>
2022-10-22 19:03:06 +0900

    22, Oct, 2022

---
+++ src/Q11720.java
@@ -0,0 +1,17 @@
+//Baekjoon Question 11720
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Q11720 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int trial = scanner.nextInt();
+        scanner.nextLine();
+        int sum = 0;
+        String data = scanner.nextLine();
+        for (int i = 0 ; i < trial ; i++){
+            sum += ((int) data.charAt(i) -'0');
+        }
+        System.out.println(sum);
+    }
+}
