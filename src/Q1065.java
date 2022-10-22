ff9ddd089f060fe4b06fb9c93939c6ce3c716132
judepark <psw0616a@naver.com>
2022-10-22 17:32:05 +0900

    22, Oct, 2022

---
+++ src/Q1065.java
@@ -0,0 +1,23 @@
+//Baekjoon Question1065
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Q1065 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        int count = 0;
+        for (int i = 1; i <= input ; i++){
+            if(i < 100)
+                count++;
+            else{
+            int digit1 = (i/100)-(i%100)/10;
+            int digit2 = (i%100)/10 - (i%10);
+            if(digit2 == digit1){
+                count++;}
+            }
+        }
+        System.out.println(count);
+    }
+}
---
+++ src/Q11654.java
@@ -0,0 +1,10 @@
+//Question 11654
+import java.util.Scanner;
+public class Q11654 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char input = scanner.next().charAt(0);
+        int asc = input;
+        System.out.println(asc);
+    }
+}
