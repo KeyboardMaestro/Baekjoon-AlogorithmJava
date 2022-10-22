7c6aa71f341d05a52252b7b8174a52ea20c64d0d
judepark <psw0616a@naver.com>
2022-10-22 19:29:31 +0900

    22, Oct, 2022

---
+++ src/Q10809.java
@@ -0,0 +1,15 @@
+//Baekjoon Question10809
+import java.util.Scanner;
+
+public class Q10809 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputdata = scanner.nextLine();
+        for (char i = 'a'; i<= 'z'; i++ ){
+            if (inputdata.indexOf(i) == -1){
+                System.out.print(inputdata.indexOf(i)+" ");
+            }else
+                System.out.print(inputdata.indexOf(i)+" ");
+        }
+    }
+}
