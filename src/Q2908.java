36cf6f9fa13c9842db0e6770fb3ed28ab86cead9
judepark <psw0616a@naver.com>
2022-11-13 21:05:14 +0900

    13, Nov, 2022

---
+++ src/Q2908.java
@@ -0,0 +1,17 @@
+//Baekjoon Question 2908
+import java.util.*;
+
+public class Q2908 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        String lists = "";
+        int[] answer;
+        for (int i = 0; i < string.length(); i++){
+            lists += string.charAt(string.length()-1-i);
+        }
+        String[] answers = lists.split(" ");
+        answer = Arrays.stream(answers).mapToInt(Integer::parseInt).toArray();
+        System.out.println(Arrays.stream(answer).max().getAsInt());
+    }
+}
