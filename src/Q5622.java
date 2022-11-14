c68d3f4c9500a66dc7b0c75219b65ae13f0a5a93
judepark <psw0616a@naver.com>
2022-11-14 13:16:08 +0900

    14, Nov, 2022

--- src/Q5622.java
+++ src/Q5622.java
@@ -8,85 +8,33 @@ public class Q5622 {
         String input = scanner.nextLine();
         int answer = 0;
         for (int i = 0; i < input.length(); i++) {
-            switch (input.charAt(i)){
-                case 'A':
-                    answer += 3;
-                    break;
-                case 'B':
-                    answer += 3;
-                    break;
-                case 'C':
-                    answer += 3;
-                    break;
-                case 'D':
-                    answer += 4;
-                    break;
-                case 'E':
-                    answer += 4;
-                    break;
-                case 'F':
-                    answer += 4;
-                    break;
-                case 'G':
-                    answer += 5;
-                    break;
-                case 'H':
-                    answer += 5;
-                    break;
-                case 'I':
-                    answer += 5;
-                    break;
-                case 'J':
-                    answer += 6;
-                    break;
-                case 'K':
-                    answer += 6;
-                    break;
-                case 'L':
-                    answer += 6;
-                    break;
-                case 'M':
-                    answer += 7;
-                    break;
-                case 'N':
-                    answer += 7;
-                    break;
-                case 'O':
-                    answer += 7;
-                    break;
-                case 'P':
-                    answer += 8;
-                    break;
-                case 'Q':
-                    answer += 8;
-                    break;
-                case 'R':
-                    answer += 8;
-                    break;
-                case 'S':
-                    answer += 8;
-                    break;
-                case 'T':
-                    answer += 9;
-                    break;
-                case 'U':
-                    answer += 9;
-                    break;
-                case 'V':
-                    answer += 9;
-                    break;
-                case 'W':
-                    answer += 10;
-                    break;
-                case 'X':
-                    answer += 10;
-                    break;
-                case 'Y':
-                    answer += 10;
-                    break;
-                case 'Z':
-                    answer += 10;
-                    break;
+            switch (input.charAt(i)) {
+                case 'A' -> answer += 3;
+                case 'B' -> answer += 3;
+                case 'C' -> answer += 3;
+                case 'D' -> answer += 4;
+                case 'E' -> answer += 4;
+                case 'F' -> answer += 4;
+                case 'G' -> answer += 5;
+                case 'H' -> answer += 5;
+                case 'I' -> answer += 5;
+                case 'J' -> answer += 6;
+                case 'K' -> answer += 6;
+                case 'L' -> answer += 6;
+                case 'M' -> answer += 7;
+                case 'N' -> answer += 7;
+                case 'O' -> answer += 7;
+                case 'P' -> answer += 8;
+                case 'Q' -> answer += 8;
+                case 'R' -> answer += 8;
+                case 'S' -> answer += 8;
+                case 'T' -> answer += 9;
+                case 'U' -> answer += 9;
+                case 'V' -> answer += 9;
+                case 'W' -> answer += 10;
+                case 'X' -> answer += 10;
+                case 'Y' -> answer += 10;
+                case 'Z' -> answer += 10;
             }
         }
         System.out.println(answer);
