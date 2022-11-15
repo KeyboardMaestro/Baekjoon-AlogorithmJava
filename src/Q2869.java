f9ecc93dfdbee71be2ccae532bac5d106f189f59
judepark <psw0616a@naver.com>
2022-11-15 23:48:10 +0900

    15, Nov, 2022

---
+++ src/Q2869.java
@@ -0,0 +1,17 @@
+//Baekjoon Question 2869
+
+import java.io.*;
+
+public class Q2869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] numbers = reader.readLine().split(" ");
+        long a = Integer.parseInt(numbers[0]);
+        long b = Integer.parseInt(numbers[1]);
+        long v = Integer.parseInt(numbers[2]);
+        double days;
+        days = (double) (v-b) / (double) (a-b);
+        days = Math.ceil(days);
+        System.out.println((long)days);
+    }
+}
