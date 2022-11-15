79278b5c374d9ec8a8990db340c6860f1d32fd00
judepark <psw0616a@naver.com>
2022-11-15 15:54:15 +0900

    15, Nov, 2022

---
+++ src/Q1712.java
@@ -0,0 +1,20 @@
+//Baekjoon Question 1712
+import java.util.Scanner;
+
+public class Q1712 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        long sell = 0;
+        if (c <= b){
+            sell = -1;
+        }
+        else {
+            sell = a / (c-b) + 1;
+        }
+
+        System.out.println(sell);
+    }
+}
