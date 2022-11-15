d064fc82be7129101e97da7c4a65392316c4a1cb
judepark <psw0616a@naver.com>
2022-11-15 17:31:55 +0900

    15, Nov, 2022

---
+++ src/Q2292.java
@@ -0,0 +1,22 @@
+//Baekjoon Question2292
+import java.util.Scanner;
+
+public class Q2292 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long input = scanner.nextInt();
+        long rule = 1;
+        long point = 1;
+        if(input == 1 )
+            System.out.println(1);
+        else if (input <= 7)
+            System.out.println(2);
+        else {
+            do {
+                point = point + (6 * (rule));
+                rule++;
+            } while (point < input);
+            System.out.println(rule);
+        }
+    }
+}
