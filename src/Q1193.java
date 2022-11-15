ec916a9544dbf44d2aee30b919f4d344d024623d
judepark <psw0616a@naver.com>
2022-11-15 19:39:41 +0900

    15, Nov, 2022

---
+++ src/Q1193.java
@@ -0,0 +1,24 @@
+//Baekjoon Question 1193
+import java.util.Scanner;
+
+public class Q1193 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        int sum = 0;
+        int steps = 0;
+        int son = 1, mother = 1;
+        while ( input > sum){
+            steps++;
+            sum += steps;
+        }
+        for (int i = 0; i< steps - (sum - input); i++) {
+            mother = 1 + i;
+            son = steps - i;
+        }
+        if (steps % 2 == 1)
+            System.out.println(son+"/"+mother);
+        else
+            System.out.println(mother+"/"+son);
+    }
+}
